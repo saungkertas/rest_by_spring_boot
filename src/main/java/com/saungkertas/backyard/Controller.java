af3541ff63eff83ce30921b87f4efb40446dfd21
syarifhidayatullah <syarif.h@go-jek.com>
2018-09-18 14:06:03 +0700

    Updated Controller, added postjson method to forward json post/get

--- src/main/java/com/saungkertas/backyard/Controller.java
+++ src/main/java/com/saungkertas/backyard/Controller.java
@@ -1,7 +1,6 @@
 package com.saungkertas.backyard;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
@@ -9,5 +8,10 @@ public class Controller {
     @RequestMapping("/")
     public String index() {
         return "Greetings";
+    }
+
+    @RequestMapping("/postjson")
+    public String postjson(@RequestBody String postjson) {
+        return postjson;
     }
 }
